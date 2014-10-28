a2aa0a90f8b903ccc29e9dff3e607f96371617ca
HuskyBin <xiang.zh@husky.neu.edu>
2014-10-28 14:28:33 -0400

    Create EighteenTwo.java

---
+++ EighteenTwo.java
@@ -0,0 +1,47 @@
+/*
+Perfect Shuffle
+*/
+
+import java.util.Random;
+
+public class EighteenTwo {
+    public int[] perfectShuffle(int[] cards) {
+        if (cards == null || cards.length == 0) {
+            return cards;
+        }
+
+        perfectShuffleCore(cards, cards.length - 1);
+        return cards;
+    }
+
+    private void perfectShuffleCore(int[] cards, int number) {
+        if (number == 0) {
+            return;
+        }
+
+        perfectShuffleCore(cards, number - 1);
+        int randomIndex = getRandomNumber(0, number - 1);
+        int temp = cards[number];
+        cards[number] = cards[randomIndex];
+        cards[randomIndex] = temp;
+        return;
+    }
+
+    private int getRandomNumber(int start, int end) {
+        int result = new Random().nextInt(end - start + 1) + start;
+        // int result = (int)(new Math.random() * (end - start + 1)) + start;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        EighteenTwo obj = new EighteenTwo();
+        int[] cards = new int[52];
+        for (int i = 1; i <= 52; i++) {
+            cards[i - 1] = i;
+        }
+        cards = obj.perfectShuffle(cards);
+        for (int i : cards) {
+            System.out.print(i + "  ");
+        }
+    }
+}
